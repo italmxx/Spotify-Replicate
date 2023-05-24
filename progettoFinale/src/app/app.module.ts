2bbc7f18aa1deac89ec42d2980c162e1d533e073
Giulia <Giulia@MacBook-Pro.local>
2023-05-24 09:15:50 +0200

    modifiche alla navSuperiore

--- progettoFinale/src/app/app.module.ts
+++ progettoFinale/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { NavSuperioreComponent } from './Components/nav-superiore/nav-superiore.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    NavSuperioreComponent
   ],
   imports: [
     BrowserModule,
