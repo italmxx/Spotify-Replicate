01c874e97b40f0b716bb36b5aed6b3f536f70336
Italo Mollo <italo.mollo@gmail.com>
2023-05-25 18:10:51 +0200

    Components fixed

--- progettoFinale/src/app/app-routing.module.ts
+++ progettoFinale/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { PageLoginComponent } from './page-login/page-login.component';
+import { PagePlaylistComponent } from './page-playlist/page-playlist.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'pageLogin', component: PageLoginComponent }
+  { path: 'pageLogin', component: PageLoginComponent },
+  { path: 'pagePlaylist', component: PagePlaylistComponent }
 ];
 
 @NgModule({
