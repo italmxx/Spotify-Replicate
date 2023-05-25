0aa0dff60fa5d754441528d6dc5833ab3272966f
fabioodebiase <debiasefabio76@gmail.com>
2023-05-25 15:29:28 +0200

    MODIFICA PLAYLIST PAGE

--- progettoFinale/src/app/app-routing.module.ts
+++ progettoFinale/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { PagePlaylistComponent } from './page-playlist/page-playlist.component';
+import { HomeComponent } from './home/home.component';
+import { PageLoginComponent } from './page-login/page-login.component';
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'pageLogin', component: PageLoginComponent },
+  { path: 'pagePlaylist', component: PagePlaylistComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- progettoFinale/src/app/song/song.component.ts
+++ progettoFinale/src/app/song/song.component.ts
@@ -6,9 +6,9 @@ import { Component, Input} from '@angular/core';
   styleUrls: ['./song.component.css']
 })
 export class SongComponent {
+  @Input () img_song: any | undefined;
   @Input() titolo_song: String | undefined;
   @Input () autore_song: String | undefined;
-  @Input () img_song: any | undefined;
   @Input () album_song: String | undefined;
   @Input () len_song: String | undefined;
 
