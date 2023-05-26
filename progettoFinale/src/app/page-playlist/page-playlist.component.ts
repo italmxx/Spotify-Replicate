63647a9618cdec346888319aec75de3446c0e67e
Italo Mollo <italo.mollo@gmail.com>
2023-05-26 15:41:35 +0200

    audio push

--- progettoFinale/src/app/page-playlist/page-playlist.component.ts
+++ progettoFinale/src/app/page-playlist/page-playlist.component.ts
@@ -9,10 +9,15 @@ export class PagePlaylistComponent {
   visibile : boolean = true;
   mostraImmagineCuore() {
     this.visibile = !this.visibile;
-    
+
   }
-  
- 
+  audioFile: string = 'assets/2.mp3';
+
+  playAudioOnClick() {
+    const audio = new Audio(this.audioFile);
+
+    audio.play();
+  }
 
 
 }
